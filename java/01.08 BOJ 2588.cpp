0e7f4b88f6fc378d16981fd2d5685f3953b37a4b
pkc1088 <84122393+pkc1088@users.noreply.github.com>
2022-01-08 22:54:20 +0900

    Add files via upload

---
+++ java/01.08 BOJ 2588.cpp
@@ -0,0 +1,19 @@
+import java.util.*;
+
+import static java.lang.Long.valueOf;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String x = sc.nextLine().trim();
+        String y = sc.nextLine().trim();
+        int sum = 0;
+        for (int i = 2, j = 1; i >= 0; i--) {
+            int ans = Integer.parseInt(x) * (y.charAt(i)-'0');
+            System.out.println(ans);
+            sum += ans * j;
+            j*=10;
+        }
+        System.out.println(sum);
+    }
+}
