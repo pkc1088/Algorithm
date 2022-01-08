602578498c0f63ff1ed884b59f27234ab783c301
pkc1088 <84122393+pkc1088@users.noreply.github.com>
2022-01-09 01:37:34 +0900

    Add files via upload

---
+++ java/01.09 BOJ 4673.cpp
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> v = new ArrayList<>();
+        for (int i = 0; i < 10000; i++) {
+            int x = i;
+            String s = String.valueOf(i);
+            for (int j = 0; j < s.length(); j++) {
+                x += s.charAt(j)-'0';
+            }
+            v.add(x);
+        }
+        for (int i = 0; i < 10000; i++) {
+            if(v.contains(i))
+               continue;
+            else
+                System.out.println(i);
+        }
+    }
+}
