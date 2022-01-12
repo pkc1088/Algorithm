583fcd95bb5bba8894711cc3037102119fd87fb1
pkc1088 <84122393+pkc1088@users.noreply.github.com>
2022-01-12 16:22:24 +0900

    Add files via upload

---
+++ java/01.12 BOJ 3053.cpp
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        double r = Double.parseDouble(st.nextToken());
+        bw.write(String.format("%.6f", (double)r*r*3.14159265358979323846));
+        bw.newLine();
+        bw.write(String.format("%.6f", (double)r*r*2.000000));
+
+
+        bw.flush();
+        bw.close();
+    }
+}
+
