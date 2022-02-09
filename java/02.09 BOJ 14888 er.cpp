a0cb6d631121be0abf6ea12a12637c8d7d017f34
pkc1088 <84122393+pkc1088@users.noreply.github.com>
2022-02-09 13:02:18 +0900

    Add files via upload

---
+++ java/02.09 BOJ 14888 er.cpp
@@ -0,0 +1,133 @@
+import java.util.*;
+/*
+public class Main {
+ 
+	public static int MAX = Integer.MIN_VALUE;	// 최댓값 
+	public static int MIN = Integer.MAX_VALUE;	// 최솟값 
+	public static int[] operator = new int[4];	// 연산자 개수 
+	public static int[] number;					// 숫자 
+	public static int N;						// 숫자 개수 
+ 
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		N = Integer.parseInt(br.readLine());
+		number = new int[N];
+ 
+		// 숫자 입력 
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < N; i++) {
+			number[i] = Integer.parseInt(st.nextToken());
+		}
+ 
+		// 연산자 입력 
+		st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < 4; i++) {
+			operator[i] = Integer.parseInt(st.nextToken());
+		}
+ 
+		dfs(number[0], 1);
+ 
+		System.out.println(MAX);
+		System.out.println(MIN);
+ 
+	}
+ 
+	public static void dfs(int num, int idx) {
+		if (idx == N) {
+			MAX = Math.max(MAX, num);
+			MIN = Math.min(MIN, num);
+			return;
+		}
+ 
+		for (int i = 0; i < 4; i++) {
+			// 연산자 개수가 1개 이상인 경우
+			if (operator[i] > 0) {
+ 
+				// 해당 연산자를 1 감소시킨다.
+				operator[i]--;
+ 
+				switch (i) {
+ 
+				case 0:	dfs(num + number[idx], idx + 1);	break;
+				case 1:	dfs(num - number[idx], idx + 1);	break;
+				case 2:	dfs(num * number[idx], idx + 1);	break;
+				case 3:	dfs(num / number[idx], idx + 1);	break;
+ 
+				}
+				// 재귀호출이 종료되면 다시 해당 연산자 개수를 복구한다.
+				operator[i]++;
+			}
+		}
+	}
+ 
+}
+*/
+public class Main {
+
+    public static int n;
+    // 연산을 수행하고자 하는 수 리스트
+    public static ArrayList<Integer> arr = new ArrayList<>();
+    // 더하기, 빼기, 곱하기, 나누기 연산자 개수
+    public static int add, sub, mul, divi;
+    
+    // 최솟값과 최댓값 초기화
+    public static int minValue = (int) 1e9;
+    public static int maxValue = (int) -1e9;
+
+    // 깊이 우선 탐색 (DFS) 메서드
+    public static void dfs(int i, int now) {
+        // 모든 연산자를 다 사용한 경우, 최솟값과 최댓값 업데이트
+        if (i == n) {
+            minValue = Math.min(minValue, now);
+            maxValue = Math.max(maxValue, now);
+        }
+        else {
+            // 각 연산자에 대하여 재귀적으로 수행
+            if (add > 0) {
+                add -= 1;
+                dfs(i + 1, now + arr.get(i));
+                add += 1;
+            }
+            if (sub > 0) {
+                sub -= 1;
+                dfs(i + 1, now - arr.get(i));
+                sub += 1;
+            }
+            if (mul > 0) {
+                mul -= 1;
+                dfs(i + 1, now * arr.get(i));
+                mul += 1;
+            }
+            if (divi > 0) {
+                divi -= 1;
+                dfs(i + 1, now / arr.get(i));
+                divi += 1;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            arr.add(x);
+        }
+
+        add = sc.nextInt();
+        sub = sc.nextInt();
+        mul = sc.nextInt();
+        divi = sc.nextInt();
+
+        // DFS 메서드 호출
+        dfs(1, arr.get(0));
+
+        // 최댓값과 최솟값 차례대로 출력
+        System.out.println(maxValue);
+        System.out.println(minValue);
+    }
+}
---
+++ java/02.09 BOJ 18428 ERR.cpp
@@ -0,0 +1,223 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static int N;
+    static char[][] map;
+    static int moveR[] = { -1, 1, 0, 0 };
+    static int moveC[] = { 0, 0, -1, 1 };
+    static int answer;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        map = new char[N][N];
+
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<N; j++) {
+                String s = sc.next();
+                map[i][j] = s.charAt(0);
+            }
+
+        }
+
+        dfs(0, -1, 0);
+
+        if(answer == 1)
+            System.out.println("YES");
+        else
+            System.out.println("NO");
+    }
+
+    static void dfs(int r, int c, int cnt) {
+        if(cnt == 3) {
+            boolean yes = true;
+
+            for(int i=0; i<N; i++) {
+                for(int j=0; j<N; j++) {
+                    if(map[i][j] != 'T')
+                        continue;
+
+                    // 감시 가능한 학생이 존재하는지
+                    boolean sw = findS(i, j);
+                    // 감시 가능
+                    if(sw == true) {
+                        yes = false;
+                        break;
+                    }
+                }
+                // 감시 가능
+                if(yes == false)
+                    break;
+            }
+            // 감시 불가
+            if(yes == true)
+                answer = 1;
+            return;
+        }
+
+        for(int i=c+1; i<N; i++) {  //처음에 i=0 초기화   //가로로 체크
+            if(map[r][i] != 'X')
+                continue;
+
+            map[r][i] = 'O';
+            dfs(r, i, cnt+1);
+            map[r][i] = 'X';
+        }
+
+        for(int i=r+1; i<N; i++) {  //처음에 i=1 초기화   //세로롤 체크
+            for(int j=0; j<N; j++) {
+                if(map[i][j] != 'X')
+                    continue;
+                map[i][j] = 'O';
+                dfs(i, j, cnt+1);
+                map[i][j] = 'X';
+            }
+        }
+    }
+
+    static boolean findS(int r, int c) {
+
+        for(int i=0; i<4; i++) {
+            int nextR = r;
+            int nextC = c;
+            while(true) {
+                nextR += moveR[i];
+                nextC += moveC[i];
+                if(nextR<0 || nextR>=N || nextC<0 || nextC>=N)
+                    break;
+                if(map[nextR][nextC] == 'O')
+                    break;
+
+                if(map[nextR][nextC] == 'S')
+                    return true;
+
+            }
+        }
+
+        return false;
+    }
+
+}
+
+
+
+//내 풀이 lil error? 
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static int n;
+    public static char[][] board;
+    public static char[][] temp;
+
+    public static int[] dx = {-1, 0, 1, 0};
+    public static int[] dy = {0, 1, 0, -1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+        board = new char[n][n];
+        temp = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < n; j++) {
+                board[i][j] = st.nextToken().charAt(0);
+            }
+        }
+
+        if(dfs(0))
+            System.out.println("NO");
+        else
+            System.out.println("YES");
+    }
+
+    public static boolean dfs(int count) {
+        if (count == 3) {
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    temp[i][j] = board[i][j];
+                }
+            }
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (temp[i][j] == 'T') {
+                        for (int dir = 0; dir < 4; dir++) {
+                            if(Collision_Check(i, j, dir)) {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+            
+            return false;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'X') {
+                    board[i][j] = 'O';
+                    dfs(count+1);
+                    board[i][j] = 'X';
+                }
+            }
+        }
+        return false;
+    }
+
+    public static boolean Collision_Check(int x, int y, int direction) {
+        // 왼쪽 방향으로 감시
+        if (direction == 0) {
+            while (y >= 0) {
+                if (temp[x][y] == 'S') { // 학생이 있는 경우
+                    return true;
+                }
+                if (temp[x][y] == 'O') { // 장애물이 있는 경우
+                    return false;
+                }
+                y -= 1;
+            }
+        }
+        // 오른쪽 방향으로 감시
+        if (direction == 1) {
+            while (y < n) {
+                if (temp[x][y] == 'S') { // 학생이 있는 경우
+                    return true;
+                }
+                if (temp[x][y] == 'O') { // 장애물이 있는 경우
+                    return false;
+                }
+                y += 1;
+            }
+        }
+        // 위쪽 방향으로 감시
+        if (direction == 2) {
+            while (x >= 0) {
+                if (temp[x][y] == 'S') { // 학생이 있는 경우
+                    return true;
+                }
+                if (temp[x][y] == 'O') { // 장애물이 있는 경우
+                    return false;
+                }
+                x -= 1;
+            }
+        }
+        // 아래쪽 방향으로 감시
+        if (direction == 3) {
+            while (x < n) {
+                if (temp[x][y] == 'S') { // 학생이 있는 경우
+                    return true;
+                }
+                if (temp[x][y] == 'O') { // 장애물이 있는 경우
+                    return false;
+                }
+                x += 1;
+            }
+        }
+        return false;
+    }
+
+}
