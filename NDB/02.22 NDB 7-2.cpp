6aa4aa846b28a786a92b88b3e68eb5a70113ece7
pkc1088 <84122393+pkc1088@users.noreply.github.com>
2022-02-22 16:59:53 +0900

    Add files via upload

---
+++ NDB/02.22 NDB 7-2.cpp
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int[] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int low = 0;
+        int high = arr[n-1]+1;
+        int mid;
+
+        while (low <= high) {
+            mid = (low + high) / 2;
+            int len = 0;
+
+            for (int i = 0; i < n; i++) {
+                if(arr[i] > mid)
+                    len += arr[i] - mid;
+            }
+
+            if (len < m) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+
+        }
+
+
+        System.out.println(low - 1);
+    }
+}
