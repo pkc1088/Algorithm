13cd4241bb47f7721b01ee7a533854f5f0c702f1
pkc1088 <84122393+pkc1088@users.noreply.github.com>
2022-02-23 16:20:05 +0900

    Add files via upload

---
+++ java/02.23 BOJ 11279.cpp
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) throws IOException {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if(x == 0) {
+                if(pq.peek() == null) {
+                    pq.poll();
+                    System.out.println(0);
+                }
+                else
+                    System.out.println((-1)*pq.poll());
+            }
+            else {
+                pq.add(x*(-1));
+            }
+        }
+
+    }
+}
