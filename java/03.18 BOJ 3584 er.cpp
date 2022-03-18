15a07ab847b6879540fe08a2866d264911796bac
pkc1088 <84122393+pkc1088@users.noreply.github.com>
2022-03-18 10:47:04 +0900

    Add files via upload

---
+++ java/03.18 BOJ 3584 er.cpp
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+    static List<Integer>[] list;
+    static int[] parent, depth;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+
+        for(int t=0; t<tc; t++) {
+            int n = Integer.parseInt(br.readLine());
+
+            parent = new int[n+1];
+            depth = new int[n+1];
+            list = new ArrayList[n+1];
+            for(int i=1; i<n+1; i++) {
+                list[i] = new ArrayList<>();
+            }
+
+            boolean[] rootCheck = new boolean[n+1];
+            Arrays.fill(rootCheck, true);
+            StringTokenizer st;
+            for(int i=0; i<n-1; i++) {
+                st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                list[a].add(b);
+                rootCheck[b] = false;   //a가 b의 부모이기 떄문
+            }
+            int rootIdx=0;
+            for(int i=1; i<n+1; i++) {      // 루트 노드 구하기
+                if(rootCheck[i] == true) {
+                    rootIdx = i;
+                    break;
+                }
+            }
+
+            init(rootIdx,1,0);
+
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            System.out.println(LCA(a,b));
+        }
+
+    }
+
+    static void init(int cur, int d, int pa) {  // 각 노드들에 부모 설정해주기
+        depth[cur] = d;
+        parent[cur] = pa;
+        for(int next : list[cur]) {
+            if(next != pa) {
+                init(next, d+1, cur);
+            }
+        }
+    }
+
+    static int LCA(int a, int b) {
+        int ah = depth[a];
+        int bh = depth[b];
+        while(ah > bh) {
+            a = parent[a];
+            ah--;
+        }
+
+        while(bh > ah) {
+            b = parent[b];
+            bh--;
+        }
+
+        while(a!=b) {
+            a = parent[a];
+            b = parent[b];
+        }
+
+        return a;   //b로 줘도 노상관
+    }
+}
---
+++ java/03.18 BOJ 5670 ERR.cpp
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static class TrieNode{
+        Map<Character, TrieNode> childNode = new HashMap<>();
+        boolean terminal;
+
+        public TrieNode(){}
+
+        public void insert(String word) {
+            TrieNode trieNode = this;
+            for(int i=0; i<word.length(); i++) {
+                char c = word.charAt(i);
+                // Key 값이 존재하는 경우 Map의 Value의 값을 반환하고,
+                // Key값이 존재하지 않는 경우 Key와 Value를 Map에 저장하고 Null을 반환합니다.
+                trieNode.childNode.putIfAbsent(c, new TrieNode());
+                trieNode = trieNode.childNode.get(c);
+
+                if(i==word.length()-1) {
+                    trieNode.terminal = true;
+                }
+            }
+        }
+
+        public int autoModule(String word) {
+            TrieNode trieNode = this;
+            int cnt=0;
+            for(int i=0; i<word.length(); i++) {
+                char c= word.charAt(i);
+                TrieNode node = trieNode.childNode.get(c);
+                if(i==0) {
+                    cnt++;
+                }
+                else if(trieNode.terminal || trieNode.childNode.size()>1) {
+                    cnt++;
+                }
+                trieNode = node;
+            }
+            return cnt;
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+
+        while((line = br.readLine())!=null) {
+            int n = Integer.parseInt(line);
+            List<String> inputData = new ArrayList<>();
+            TrieNode trie = new TrieNode();
+            for(int i=0; i<n; i++) {
+                String str = br.readLine();
+                inputData.add(str);
+                trie.insert(str);
+            }
+
+            double res=0;
+
+            for(String str : inputData) {
+                res+= trie.autoModule(str);
+            }
+            System.out.println(String.format("%.2f",res/inputData.size()));
+        }
+    }
+}
