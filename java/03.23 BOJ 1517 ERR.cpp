d9cb777aa1cc9d2b45d136636476ae937257c8d8
pkc1088 <84122393+pkc1088@users.noreply.github.com>
2022-03-24 12:22:37 +0900

    Update 03.23 BOJ 1517 ERR.cpp

--- java/03.23 BOJ 1517 ERR.cpp
+++ java/03.23 BOJ 1517 ERR.cpp
@@ -25,12 +25,14 @@ public class Main {
         Arrays.sort(index);
 
         tree = new long[getTreeSize()];
+
         long ans =0;
         for(int i=0; i<n; i++)	{
-            int idx = pos.get(index[i]);
-            ans += sum(0, n-1, 1, idx+1, n-1);
+            int left = pos.get(index[i]);
+            System.out.println("left : "+left);
+            ans += sum(0, n-1, 1, left+1, n-1);
 
-            update(0, n-1, 1, idx, 1);
+            update(0, n-1, 1, left, 1);
         }
         System.out.println(ans);
     }
@@ -41,31 +43,94 @@ public class Main {
     }
 
     static long sum(int start, int end, int node, int left, int right) {
-
+        System.out.println("sum : "+start+", "+end+", "+node+", "+left+", "+right);
         if(end < left || right < start )
             return 0;
-        if(left <= start && end <= right)
+        if(left <= start && end <= right) {
+            System.out.println("tree[node] : "+tree[node]);
             return tree[node];
-
-        int mid = (start+end)/2;
-        return sum(start, mid, node*2, left, right) +
-               sum(mid+1, end, node*2+1, left, right);
-    }
-
-    static void update(int start, int end, int node, int idx, int dif) {
-        if(start == end ) {
-            tree[node] = dif;
-            return;
         }
 
         int mid = (start+end)/2;
+        return sum(start, mid, node*2, left, right) +
+                sum(mid+1, end, node*2+1, left, right);
+    }
 
-        if(idx <= mid)
-            update(start, mid, node*2, idx, dif);
-        else
-            update(mid+1, end, node*2+1, idx, dif);
+    static void update(int start, int end, int node, int idx, int dif) {
+        System.out.println("update : "+start+", "+end+", "+node+", "+idx+", "+dif);
+        if (idx < start || idx > end) {
+            return;
+        }
 
-        tree[node] = tree[node*2]+tree[node*2+1];
+        // 범위 안에 있으면 내려가며 다른 원소도 갱신
+        tree[node] += dif;
+        if (start == end) {
+            return;
+        }
+
+        int mid = (start + end) / 2;
+        update(start, mid, node * 2, idx, dif);
+        update(mid + 1, end, node * 2 + 1, idx, dif);
     }
 
 }
+
+/*
+class Main {
+    static int stoi(String s) { return Integer.parseInt(s);}
+
+    static long swapCount = 0;
+    static long[] sorted;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = stoi(br.readLine());
+        sorted = new long[n];
+        long[] arr = new long[n];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<n; i++)
+            arr[i] = stoi(st.nextToken());
+
+        mergeSort(arr, 0, n-1);
+
+        System.out.println(swapCount);
+    }
+
+    static void mergeSort(long[] arr, int low, int high) {
+        if(low < high) {
+            int mid = (low+high)/2;
+            mergeSort(arr, low, mid);
+            mergeSort(arr, mid+1, high);
+            merge(arr, low, mid, high);
+        }
+    }
+
+    static void merge(long[] arr, int low, int mid, int high) {
+        int i = low;
+        int j = mid + 1;
+        int index = low;
+
+        while(i <= mid && j <= high) {
+            if(arr[i] <= arr[j])
+                sorted[index++] = arr[i++];
+            else {
+                sorted[index++] = arr[j++];
+                swapCount += (mid + 1 - i);
+            }
+        }
+
+        while(i <= mid)
+            sorted[index++] = arr[i++];
+
+        while(j <= high)
+            sorted[index++] = arr[j++];
+
+        for(int k=low; k<=high; k++)
+            arr[k] = sorted[k];
+    }
+}
+
+ */
