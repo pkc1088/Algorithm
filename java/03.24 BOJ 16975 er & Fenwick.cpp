b705f32448f2637b872b2b4f3b532b25f9dc1d22
pkc1088 <84122393+pkc1088@users.noreply.github.com>
2022-03-25 10:45:12 +0900

    Update and rename 03.24 BOJ 16975 er.cpp to 03.24 BOJ 16975 er & Fenwick.cpp

--- java/03.24 BOJ 16975 er & Fenwick.cpp
+++ java/03.24 BOJ 16975 er & Fenwick.cpp
@@ -4,7 +4,7 @@ import java.util.*;
 public class Main {
     private static int[] arr;
     private static long[] sumtree;
-    //arrÀ» ¹üÀ§½ºÄµÇÏ¸é ½Ã°£ÃÊ°ú µÊ
+    //arrì„ ë²”ìœ„ìŠ¤ìº”í•˜ë©´ ì‹œê°„ì´ˆê³¼ ë¨
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
@@ -30,7 +30,7 @@ public class Main {
                 update(1, N, 1, v + 1, -k);
             } else {
                 int v = Integer.parseInt(st.nextToken());
-                long result = getSum(1, N, 1, 1, v) + arr[v];   //º¯È­·® + arr±âÁ¸°ª
+                long result = getSum(1, N, 1, 1, v) + arr[v];   //ë³€í™”ëŸ‰ + arrê¸°ì¡´ê°’
                 sb.append(result).append('\n');
             }
         }
@@ -62,3 +62,60 @@ public class Main {
     }
 }
 
+/*
+public class Main {
+
+	static long[] tree;
+	static int n;
+	static void add(int pos, int val) {
+		while(pos <= n) {
+			tree[pos] += val;
+			pos += (pos&-pos);
+		}
+	}
+	
+	static long sum(int pos) {
+		long result = 0;
+		while(pos > 0) {
+			result += tree[pos];
+			pos &= (pos-1);
+		}
+		return result;
+		
+	}
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		tree = new long[n+1];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int prev = Integer.parseInt(st.nextToken());
+		int now;
+		add(1, prev);
+		for(int i=2; i<=n; i++) {
+			now = Integer.parseInt(st.nextToken());
+			add(i, now - prev);
+			prev = now;
+		}
+		StringBuilder sb = new StringBuilder();
+		int m = Integer.parseInt(br.readLine());
+		for(int i=0; i<m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int op = Integer.parseInt(st.nextToken());
+			int a = Integer.parseInt(st.nextToken());
+			
+			if(op == 1) {
+				int b = Integer.parseInt(st.nextToken());
+				int k = Integer.parseInt(st.nextToken());
+				
+				add(a, k);
+				add(b+1, -k);
+			}else {
+				sb.append(sum(a)+"\n");
+			}
+            
+		}
+		System.out.println(sb.toString());
+		
+	}
+}
+*/
