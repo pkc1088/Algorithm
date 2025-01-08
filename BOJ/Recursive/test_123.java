79c0f3eeddaace5c8b3976c926a7c5aaf529a2d1
pkc1088 <pkcmax@naver.com>
2025-01-08 16:58:00 +0900

    committed via intJ term

--- BOJ/Recursive/test_123.java
+++ BOJ/Recursive/test_123.java
@@ -2,6 +2,6 @@ package BOJ_Quiz.Recursive;
 
 public class test_123 {
     public static void main(String[] args) {
-        System.out.println("there 2");
+        System.out.println("there 2/3");
     }
 }
